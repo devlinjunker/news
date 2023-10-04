f59b71d6d8e6b29a1a3d203a8131c1f337b1aa4d
Devlin Junker <devlin.junker@gmail.com>
2023-10-04 15:18:07 -0700

    add ErrorBoundary class
    Signed-off-by: Devlin Junker <devlin.junker@gmail.com>

---
+++ src/components/utils/ErrorBoundary.vue
@@ -0,0 +1,32 @@
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+	props: {
+		// eslint-disable-next-line vue/require-default-prop, vue/require-prop-types
+		watch: {
+			required: false,
+		},
+	},
+	data: () => ({
+		error: false,
+	}),
+	watch: {
+		watch() {
+			this.error = false
+		},
+	},
+	errorCaptured(/* err, vm, info */) {
+		this.error = true
+		return false
+	},
+	render(h) {
+		if (this.error) {
+			return this.$slots?.error ? this.$slots?.error[0] : h('p', t('news', 'Something went wrong'))
+		} else {
+			return this.$slots.default[0]
+		}
+	},
+})
+
+</script>
